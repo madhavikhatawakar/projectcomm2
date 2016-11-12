0f4e9e0a32065e978ca7a3d90adbf7fa67591182
root <root@ip-172-31-28-33.ec2.internal>
2016-11-12 13:15:09 +0000

    JIRAISSUE Added magento code

---
+++ magento/app/code/local/CybALM/HelloWorld/controllers/FooController.php
@@ -0,0 +1,15 @@
+<?php
+class CybALM_HelloWorld_FooController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo 'Foo Index Action';
+    }
+
+    public function addAction(){
+        echo 'Foo add Action';
+    }
+
+    public function deleteAction(){
+        echo 'Foo delete Action';
+    }
+}
+?>
---
+++ magento/app/code/local/MyCompanyName/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+class CybALM_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo "We're echoing just to show that this is what's called, normally you'd have some kind of redirect going on here";
+    }
+}
+?>
